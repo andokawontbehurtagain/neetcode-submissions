0e6ffba3324e99e368fc4ae7d975f05867edd65a
andoka <xtalefrisk2206@gmail.com>
2026-09-15 17:58:59 +0700

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i=digits.size()-1;i>=0;i--){
+            digits[i]++;
+             if(digits[i]<10){
+               return digits;}
+                if(digits[i]==10){
+                    digits[i]=0;
+    
+                }
+               
+             }
+             digits.insert(digits.begin(), 1);
+             return digits;
+
+             
+        
+    }
+};
